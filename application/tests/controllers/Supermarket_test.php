f6b7ac484f4feb90479397462ef1857fffe522b9
Rushikesh <rushikeshghorpade49@gmail.com>
2021-05-11 19:26:33 +0530

    Test case added

--- application/tests/controllers/Supermarket_test.php
+++ application/tests/controllers/Supermarket_test.php
@@ -6,5 +6,10 @@ class Supermarket_test extends TestCase
 	{
 		$output = $this->request('GET', 'supermarket/index');
 		$this->assertStringContainsString('<title>Welcome to supermarket checkout</title>', $output);
-	}	
+	}
+    
+    public function test_autocomplete(){
+        $output = $this->request('GET', 'supermarket/get_autocomplete?term=A');
+		$this->assertStringContainsString('A', $output);
+    }
 }
